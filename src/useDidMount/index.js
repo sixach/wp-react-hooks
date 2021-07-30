5bf03af4737ed2f1b7a050289f525f01045f9353
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-07-30 22:33:33 +0300

    `componentDidMount` hook for React

---
+++ src/useDidMount/index.js
@@ -0,0 +1,30 @@
+/**
+ * WordPress specific abstraction layer atop React.
+ *
+ * @see    https://developer.wordpress.org/block-editor/reference-guides/packages/packages-element/
+ * @ignore
+ */
+import { useEffect } from '@wordpress/element';
+
+/**
+ * This hook mimicks the `componentDidMount` hook for React.
+ *
+ * @function
+ * @since      1.0.0
+ * @param  	   {Function}    callback    Function to be called when component is mounted.
+ * @return     {void}
+ * @example
+ *
+ * useDidMount( () => {
+ *     console.log( 'Mounted!' );
+ * } );
+ */
+function useDidMount( callback ) {
+	useEffect( () => {
+		if ( isFunction( callback ) ) {
+			callback();
+		}
+	}, [] );
+}
+
+export default useDidMount;
