d3f79164f12a289f56e618e53a7b56047cdb7547
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-08-26 22:26:06 +0300

    Exported newly added hooks

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 export { default as useDidMount } from './useDidMount';
 export { default as useDidUpdate } from './useDidUpdate';
+export { default as useGetPosts } from './useGetPosts';
+export { default as useGetTerms } from './useGetTerms';
 export { default as useInputValue } from './useInputValue';
+export { default as usePreparePosts } from './usePreparePosts';
 export { default as useTimeout } from './useTimeout';
 export { default as useToggle } from './useToggle';
