87f13dab994537f170aa3994c3617c6c725fe244
Mahdi Yazdani <mahdiyazdani@msn.com>
2021-09-16 00:43:31 +0300

    Exported new hooks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+export { default as useActiveTab } from './useActiveTab';
+export { default as useConditionalRef } from './useConditionalRef';
 export { default as useDidMount } from './useDidMount';
 export { default as useDidUpdate } from './useDidUpdate';
+export { default as useGetNodeList } from './useGetNodeList';
 export { default as useGetPosts } from './useGetPosts';
 export { default as useGetTerms } from './useGetTerms';
 export { default as useInputValue } from './useInputValue';
+export { default as useLatLngBounds } from './useLatLngBounds';
 export { default as usePreparePosts } from './usePreparePosts';
 export { default as useTimeout } from './useTimeout';
 export { default as useToast } from './useToast';
